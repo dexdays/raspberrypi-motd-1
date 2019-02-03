eacff56b4b21f42396cd2d3e68f121adce105af5
Mᴀʀsʜᴀʟʟ Mʏᴛʜ <32271330+marshallmyth@users.noreply.github.com>
2019-02-03 19:38:21 +0100

    One Line Installation

--- motdinstall.sh
+++ motdinstall.sh
@@ -1,1 +1,2 @@
+#one line installation
 git clone -q https://github.com/marshallmyth/raspberrypimotd && cd raspberrypimotd && sudo cp motd.sh /etc/profile.d/ && sudo chown root:root motd.sh && sudo chmod +x motd.sh && sudo rm /etc/motd ; sudo sed -i "s/#PrintLastLog .*/PrintLastLog no/1" /etc/ssh/sshd_config && sudo systemctl restart ssh && cd .. && rm -rf raspberrypimotd && echo " " && echo "Done!"
