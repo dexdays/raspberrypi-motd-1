753aec4899049c67b51d4ba8a523002ca3473907
Mᴀʀsʜᴀʟʟ Mʏᴛʜ <32271330+marshallmyth@users.noreply.github.com>
2019-02-04 22:56:42 +0100

    Beautified

--- motdupdate.sh
+++ motdupdate.sh
@@ -1,2 +1,2 @@
 #one line update
-echo "Downloading the file..." && git clone -q https://github.com/marshallmyth/raspberrypi-motd && cd raspberrypi-motd && echo "Copying the file into /etc/porfil.d/..." && sudo cp motd.sh /etc/profile.d/ && echo "Restarting SSH service..." && sudo systemctl restart ssh && echo "Cleaning up..." && cd .. && sudo rm -rf raspberrypi-motd && echo " " && echo "Done!"
+echo "Downloading the file..." && git clone -q https://github.com/marshallmyth/raspberrypi-motd && cd raspberrypi-motd && echo "Copying the file into /etc/profile.d/..." && sudo cp motd.sh /etc/profile.d/ && echo "Restarting SSH service..." && sudo systemctl restart ssh && echo "Cleaning up..." && cd .. && sudo rm -rf raspberrypi-motd && echo " " && echo "Done!"
