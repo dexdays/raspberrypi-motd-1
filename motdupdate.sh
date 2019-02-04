5f2747d947b7f40bf43c4c18cfeeb2a755d5eb64
Mᴀʀsʜᴀʟʟ Mʏᴛʜ <32271330+marshallmyth@users.noreply.github.com>
2019-02-04 18:16:32 +0100

    Update motdupdate.sh

--- motdupdate.sh
+++ motdupdate.sh
@@ -1,2 +1,2 @@
 #one line update
-git clone -q https://github.com/marshallmyth/raspberrypimotd && cd raspberrypimotd && sudo cp motd.sh /etc/profile.d/ && sudo systemctl restart ssh && cd .. && sudo rm -rf raspberrypimotd && echo " " && echo "Done!"
+git clone -q https://github.com/marshallmyth/raspberrypi-motd && cd raspberrypi-motd && sudo cp motd.sh /etc/profile.d/ && sudo systemctl restart ssh && cd .. && sudo rm -rf raspberrypi-motd && echo " " && echo "Done!"
